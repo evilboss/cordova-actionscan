5946245deb042df59405c42557f258d36e46ba29
Jr Reyes <jr.evilboss@gmail.com>
2016-03-29 16:43:35 +0800

    modified new publish script file

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+path=$PWD
+echo $path
 cd platforms/android/build/outputs/apk/
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore actionscan.keystore android-release-unsigned.apk action-scan
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $path/actionscan.keystore android-release-unsigned.apk action-scan
 $ANDROID_HOME/build-tools/23.0.1/zipalign 4 \android-release-unsigned.apk action-scan.apk
