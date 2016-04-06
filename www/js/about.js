e88c13fa4edee8028c67c112bd722e25268e327f
Jr Reyes <jr.evilboss@gmail.com>
2016-04-06 13:12:01 +0800

    removed about js file

--- www/js/about.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by gilbertor on 4/6/16.
- */
-function init() {
-  $("#back").click(function () {
-    window.location.href = 'index.html'
-  });
-}
-document.addEventListener("deviceready", init, false);
